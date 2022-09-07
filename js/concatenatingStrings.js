3b5b6c6b412cd288669da88c48863e2030b97aa1
bubinbei <bubinbei@yandex.ru>
2022-09-07 13:51:30 +0400

    first v0.12 js

--- js/concatenatingStrings.js
+++ js/concatenatingStrings.js
@@ -1,2 +1,7 @@
 //Concatenating Strings
 const myStr = "This is the start." + " This is the end."; // Change this line
+
+//Concatenating Strings v2
+let myStr = "";
+myStr += "This is the first sentence."
+myStr += " This is the second sentence."
